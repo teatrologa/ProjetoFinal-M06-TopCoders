85764270024d6db5ec9082acdf4eaa157e329310
Marília Castro <marilialiliu@hotmail.com>
2022-09-17 19:20:21 -0300

    ---

--- Projeto Final M06 Top Coders/Program.cs
+++ Projeto Final M06 Top Coders/Program.cs
@@ -76,7 +76,11 @@ builder.Services.AddSwaggerGen(setup =>
 
 });
 
+
+
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
